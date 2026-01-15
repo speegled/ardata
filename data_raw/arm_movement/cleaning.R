5714f7be3cd2ef0c66d016c10a595335089626fb
Darrin Speegle <speegled@slu.edu>
2026-01-15 06:29:40 -0600

    man page for arm movement

--- data_raw/arm_movement/cleaning.R
+++ data_raw/arm_movement/cleaning.R
@@ -3,4 +3,6 @@ dd <- read.csv("data_raw/arm_movement/data_arm_movement_evaluation.csv")
 arm_movement <- dd |> 
   janitor::clean_names() |> 
   select(id, datetime, baseline_mean, passive_middle, passive_out, passive_in, active_middle, active_out, active_in, sex, age)
+arm_movement <- arm_movement |> 
+  mutate(datetime = lubridate::ymd_hms(datetime))
 save(arm_movement, file = "data/arm_movement.rda")
