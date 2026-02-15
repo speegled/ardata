3f33d14be7f6feeac5a8db83b7c7cb1f6d19ff21
Darrin Speegle <speegled@slu.edu>
2026-02-15 08:30:00 -0600

    making data factors

--- data_raw/body_ownership/cleanup.R
+++ data_raw/body_ownership/cleanup.R
@@ -1,5 +1,8 @@
 dd <- readxl::read_xlsx(here::here("data_raw/body_ownership/dt2.noout.xlsx")) |> 
   janitor::clean_names()
-
+library(tidyverse)
+dd <- dd |> 
+  mutate(across(where(is.character), factor))
 body_ownership <- dd
+
 save(body_ownership, file = "data/body_ownership.rda")
